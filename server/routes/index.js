81103caf407b7a2b5ede1ea080baf7e73be368fa
01518647 <01518647@yto.net.net>
2019-06-10 16:12:47 +0800

    f

--- server/routes/index.js
+++ server/routes/index.js
@@ -2,8 +2,10 @@
 //const keyList=require('./keyList.js');
 const face=require('./face.js');
 const up=require('./up.js');
+const ui=require('./ui.js');
 module.exports=(router)=>{
     face(router);
     up(router);
+    ui(router);
     return router
 };
---
+++ server/routes/ui.js
@@ -0,0 +1,4 @@
+
+module.exports=(router)=>{
+    router.get('/uindex_2', path.join(__dirname, "./index_2.html"));
+ }
